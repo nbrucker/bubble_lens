a6a2723e96b646ba4ae63a2c6c67740d5b6f76dd
Dimitri Dessus <iphoneretro@icloud.com>
2021-05-02 15:12:24 +0200

    ✨ Add full example app + improve

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,22 +21,20 @@ class MyHomePage extends StatelessWidget {
 	Widget build(BuildContext context) {
 		return Scaffold(
 			backgroundColor: Colors.white,
-			body: Center(
-				child: Container(
-					color: Colors.black,
-					child: BubbleLens(
-						width: 300,
-						height: 300,
-						widgets: [
-							for (var i = 0; i < 7; i++)
-								Container(
-									width: 100,
-									height: 100,
-									color: [Colors.red, Colors.green, Colors.blue][i % 3],
-								)
-						]
-					),
-				)
+			body: Container(
+				color: Colors.black,
+				child: BubbleLens(
+					width: MediaQuery.of(context).size.width,
+					height: MediaQuery.of(context).size.height,
+					widgets: [
+						for (var i = 0; i < 60; i++)
+							Container(
+								width: 100,
+								height: 100,
+								color: [Colors.red, Colors.green, Colors.blue][i % 3],
+							)
+					]
+				),
 			),
 		);
 	}
